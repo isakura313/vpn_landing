43f13b0f7a52f47ff5da5e94fb81e18802d3e625
Unknown <you@example.com>
2021-03-16 14:17:38 +0300

    asdadas

--- code.js
+++ code.js
@@ -1,15 +1,21 @@
-// alert("привет! Это я, твой первый скрипт")
-// let x = +prompt("Введите первое число")
-// let y = +prompt("Введите число номер 2")
-// alert(x+y)
+let modal_window = document.querySelector(".modal_window");
+let krest = document.querySelector("#krest");
+let faq_question = document.querySelector(".faq_question")
+let faq_answer = document.querySelector(".faq_answer")
 
+setInterval(() => {
+    modal_window.style.display = "block";
+    modal_window.className = 'modal_window animate__animated animate__zoomIn'
+}, 3000);
 
-setTimeout(() => {
-
-    
-    document.querySelector(".modal_window").style.display = "block";
-}, 1000);
-
-document.querySelector("#krest").onclick = () =>{
-    document.querySelector(".modal_window").style.display = "none";
+krest.onclick = () => {
+    modal_window.className = 'modal_window animate__animated animate__zoomOutDown';
+    // modal_window.style.display = "none";
+}
+faq_question.onclick = () => {
+    if (faq_answer.style.display == '' || faq_answer.style.display == 'none' ) {
+        faq_answer.style.display = 'block';
+    } else{
+        faq_answer.style.display = 'none';
+    }
 }
---
+++ server.js
@@ -0,0 +1,6 @@
+let car = {
+    model: 'jigul',
+    color: 'red',
+    speed: 100
+}
+console.log('По дороге едет' + car.model + 'цвета ${car.color}')
