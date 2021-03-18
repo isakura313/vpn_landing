6fb103339b54e70dfe4e06724bc80063c67dfe67
asncn <asncn@mail.ru>
2021-03-18 11:09:39 +0300

    add

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 let modal_window = document.querySelector(".modal_window");
 let krest = document.querySelector("#krest");
-let faq_question = document.querySelector(".faq_question")
-let faq_answer = document.querySelector(".faq_answer")
+let faq_question = document.querySelectorAll(".faq_question")
+let faq_answer = document.querySelectorAll(".faq_answer")
 
 setInterval(() => {
     modal_window.style.display = "block";
@@ -12,10 +12,13 @@ krest.onclick = () => {
     modal_window.className = 'modal_window animate__animated animate__zoomOutDown';
     // modal_window.style.display = "none";
 }
-faq_question.onclick = () => {
-    if (faq_answer.style.display == '' || faq_answer.style.display == 'none' ) {
-        faq_answer.style.display = 'block';
+
+faq_question[0].onclick = () => {
+    if (faq_answer[0].style.display == '' || faq_answer[0].style.display == 'none' ) {
+        faq_answer[0].style.display = 'block';
+        // alert('показался')
     } else{
-        faq_answer.style.display = 'none';
+        // alert('вызвался')
+        faq_answer[0].style.display = 'none';
     }
 }
